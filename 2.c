c3b2d49d1e7e8700c136fa9ecbbb75f87081d9e3
Roshidul Islam <rislam173019@bscse.uiu.ac.bd>
2021-12-27 15:18:34 +0600

    first commit

---
+++ 2.c
@@ -0,0 +1,10 @@
+
+
+#include<stdio.h>
+int main (){
+
+
+
+printf(" Today is Mon day \n");
+}
+
---
+++ 3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main (){
+
+
+
+printf("  This is software Lab class after mid \n");
+}
+
+
